2612bac78ae18ab82169e87bd4092bc908bf54f4
dldnwls0121 <alsdus0907@naver.com>
2025-09-02 15:40:52 +0900

    25.09.02 수출용 패키지

---
+++ Assets/Class2th (Script)/Script/ability.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ability : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public int strength = 5;
+    void Start()
+    {
+
+        Debug.Log("Strength : " +  strength);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
