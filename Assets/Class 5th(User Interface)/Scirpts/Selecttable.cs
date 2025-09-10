00218589e93c15b3894b2cddb89de1e708871350
dldnwls4148 <dldnwls4148@naver.com>
2025-09-11 00:54:03 +0900

    풀이

--- Assets/Class 5th(User Interface)/Scirpts/Selecttable.cs
+++ Assets/Class 5th(User Interface)/Scirpts/Selecttable.cs
@@ -2,26 +2,25 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Selecttable : MonoBehaviour
 {
-  
+ 
+    private Text text; 
 
+    private void Awake()
+    {
+        text = GetComponentInChildren<Text>();
+    }
     public void Enterable()
     {
-
-     
-        
+        text.fontSize = 100; 
     }
 
     public void Exit()
     {
-
-       
-        
+        text.fontSize = 75; 
     }
 
     public void Down()
     {
-
-     
-     
+        text.fontSize = 50; 
     }
 }
